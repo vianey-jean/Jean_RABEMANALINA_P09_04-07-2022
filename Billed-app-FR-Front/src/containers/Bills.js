00477c6b0627300df65e39a238c852f7affd5284
vianey.jean <vianey.jean@ymail.com>
2022-07-04 23:15:09 +0400

    Correction du Bug 1

--- Billed-app-FR-Front/src/containers/Bills.js
+++ Billed-app-FR-Front/src/containers/Bills.js
@@ -1,32 +1,35 @@
-import { ROUTES_PATH } from '../constants/routes.js'
-import { formatDate, formatStatus } from "../app/format.js"
-import Logout from "./Logout.js"
+import { ROUTES_PATH } from '../constants/routes.js';
+import { formatDate, formatStatus } from "../app/format.js";
+import Logout from "./Logout.js";
 
 export default class {
   constructor({ document, onNavigate, store, localStorage }) {
-    this.document = document
-    this.onNavigate = onNavigate
-    this.store = store
-    const buttonNewBill = document.querySelector(`button[data-testid="btn-new-bill"]`)
-    if (buttonNewBill) buttonNewBill.addEventListener('click', this.handleClickNewBill)
-    const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`)
+    this.document = document;
+    this.onNavigate = onNavigate;
+    this.store = store;
+    const buttonNewBill = document.querySelector(`button[data-testid="btn-new-bill"]`);
+    if (buttonNewBill) buttonNewBill.addEventListener('click', this.handleClickNewBill);
+    const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`);
     if (iconEye) iconEye.forEach(icon => {
-      icon.addEventListener('click', () => this.handleClickIconEye(icon))
+      icon.addEventListener('click', () => this.handleClickIconEye(icon));
     })
-    new Logout({ document, localStorage, onNavigate })
+    new Logout({ document, localStorage, onNavigate });
   }
 
   handleClickNewBill = () => {
-    this.onNavigate(ROUTES_PATH['NewBill'])
-  }
+    this.onNavigate(ROUTES_PATH['NewBill']);
+  };
 
   handleClickIconEye = (icon) => {
-    const billUrl = icon.getAttribute("data-bill-url")
-    const imgWidth = Math.floor($('#modaleFile').width() * 0.5)
-    $('#modaleFile').find(".modal-body").html(`<div style='text-align: center;' class="bill-proof-container"><img width=${imgWidth} src=${billUrl} alt="Bill" /></div>`)
-    $('#modaleFile').modal('show')
-  }
+    const billUrl = icon.getAttribute("data-bill-url");
+    const imgWidth = Math.floor($('#modaleFile').width() * 0.5);
+    $('#modaleFile').find(".modal-body").html(`<div style='text-align: center;' class="bill-proof-container"><img width=${imgWidth} src=${billUrl} alt="Bill" /></div>`);
+    $('#modaleFile').modal('show');
+  };
 
+    /**
+     * Pas besoin de couvrir cette fonction par des tests
+     */
   getBills = () => {
     if (this.store) {
       return this.store
@@ -34,8 +37,16 @@ export default class {
       .list()
       .then(snapshot => {
         const bills = snapshot
+        /**
+         * Bug 1: On trie dans l'order par date
+         */
+          .sort((a,b) => (a.date < b.date) ? 1: -1)
+          /**
+           * Fin du Correction du Bug
+           */
           .map(doc => {
             try {
+              // console.log(date);
               return {
                 ...doc,
                 date: formatDate(doc.date),
@@ -44,7 +55,7 @@ export default class {
             } catch(e) {
               // if for some reason, corrupted data was introduced, we manage here failing formatDate function
               // log the error and return unformatted date in that case
-              console.log(e,'for',doc)
+              console.log(e,'for',doc);
               return {
                 ...doc,
                 date: doc.date,
@@ -52,9 +63,9 @@ export default class {
               }
             }
           })
-          console.log('length', bills.length)
-        return bills
-      })
+          console.log('length', bills.length);
+        return bills;
+      });
     }
-  }
+  };
 }
